eb4d8124fc7b6e4e41e1cd0a0e8676fb0e4e0d23
cgonzb <cgonzb@gmail.com>
2016-09-06 18:44:31 -0300

    template de clases

---
+++ class_user.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Archivo de definicion de la clase user.
+ * Las clases se definen con Mayuscula para diferenciarlas de los metodos
+ */
+class User {
+
+    /**
+     * @property type $name Description
+     */
+    public $propiedad;
+
+    /**
+     * @method type methodName(type $paramName) Description
+     * @return type Description
+     */
+    public function funcion() {
+        
+    }
+
+}
