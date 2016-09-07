981e5761a23cce92db70f963c840c245c66796b9
jorgecabane93 <jorgecabane93@gmail.com>
2016-09-07 14:05:28 -0300

    comments

--- class_bd.php
+++ class_bd.php
@@ -18,25 +18,39 @@ class BD  {
 //     	$this->pass = $pass;
 //     	$this->db = $db;
 //     }
-
+    /**
+     * @property string $host host de la base de datos
+     * @property string $user usuario de la base de datos
+     * @property string $pass contraseña de la base de datos
+     * @property string $db nombre de la base de datos
+     * @property object $myconn conexion a bbdd
+     */
 	protected  $host = 'localhost';
 	protected  $user = 'root';
 	protected  $pass = '';
 	protected  $db = 'horari';
 	public $myconn;
     
+	/**
+	 * @method function connect() conexion a bbdd
+	 * @return object conexion a bbdd
+	 */
 	public function connect() {		
-		$con = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+			$con = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
 		if (!$con) {
 			die('Could not connect to database!');
 		}
 		else {
 			$this->myconn = $con;
-			echo 'Connection established!';
+//			echo 'Connection established!';
 		}
 		    return $this->myconn;		    
 	}
 	
+	/**
+	 * @method function close() cierra conexion a bbdd
+	 * @return void
+	 */
 	public function close() {
 		mysqli_close($myconn);
 		echo 'Connection closed!';
