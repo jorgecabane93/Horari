828457df92524253cdf974121718c395550f18c2
cgonzb <cgonzb@gmail.com>
2016-09-28 14:54:51 -0300

    si no hay centro muestra mensaje

--- logica/crud_center.php
+++ logica/crud_center.php
@@ -3,19 +3,24 @@
 /* solicitud de la clase */
 require_once dirname(dirname(__FILE__)) . '/controladores/class_center.php';
 
-$centers = new Center();
-
+$obj = new Center();
+$centers = $obj->get_all();
 echo "<table class='bordered'><tr><th>id</th><th>company_id</th><th>name</th><th>acronym</th><th>last modified</th><th>Opts</th>";
 echo "</tr>";
-foreach ($centers->get_all()as $center) {
-    echo "<tr>";
-    echo "<td>$center->id</td>";
-    echo "<td>$center->company_id</td>";
-    echo "<td>$center->name</td>";
-    echo "<td>$center->acronym</td>";
-    echo "<td>$center->lastmodified</td>";
-    echo "<td><a class='btn red'><i class='large material-icons'>delete</i></a></td>";
-    echo "</tr>";
+
+if ($centers) {
+    foreach ($centers as $center) {
+        echo "<tr>";
+        echo "<td>$center->id</td>";
+        echo "<td>$center->company_id</td>";
+        echo "<td>$center->name</td>";
+        echo "<td>$center->acronym</td>";
+        echo "<td>$center->lastmodified</td>";
+        echo "<td><a class='btn red'><i class='large material-icons'>delete</i></a></td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan=6 class='orange white-text'>No hay datos de la tabla en la base de datos</td></tr>";
 }
 echo "</table>";
 ?>
